b2098d03ee103c75dae86603eabc4d052aa6c2a1
Buda603 <buda.pakhutov@bk.ru>
2026-05-20 11:31:42 +0300

    Add files via upload

---
+++ practic/Feedback.php
@@ -0,0 +1,23 @@
+<?php require 'src(initFeedback.php' ?>
+
+<?php include 'src/header.php' ?>
+
+
+<h1> Добавить отзыв </h1>
+<?php if (!empty($error)): ?>
+    <div class="bg-warning"> <?= $error ?> </div>
+    <?php endif ?>
+
+
+    <div claas="feedback-index p-3">
+        <form id="w0" action
+class Feedback extends Entity 
+
+
+
+
+
+
+
+
+public function validate(): void 
